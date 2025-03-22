7c52c7be40a227c02d8519a244e01776a2add715
Vigneshwar B.E <34682561+vigneshwarsankar@users.noreply.github.com>
2025-03-23 04:52:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("header").innerHTML = data);
+
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("footer").innerHTML = data);
+});
